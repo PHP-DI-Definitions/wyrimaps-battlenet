3a758b53c0a1467e0dc959ca666f27d8f4b204d9
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-02-14 19:26:03 +0100

    Refactored to the new auth model

--- etc/di/battlenet-client.php
+++ etc/di/battlenet-client.php
@@ -1,16 +1,38 @@
 <?php declare(strict_types=1);
 
-use Clue\React\Redis\Client;
-use Psr\Log\LoggerInterface;
+use ApiClients\Foundation\Options as FoundationOptions;
+use ApiClients\Foundation\Transport\Options as TransportOptions;
 use React\EventLoop\LoopInterface;
-use WyriHaximus\React\Redis\WaitingClient\WaitingClient;
+use WyriMaps\BattleNet\AsyncClient;
+use WyriMaps\BattleNet\Authentication\ClientCredentials;
+use WyriMaps\BattleNet\Middleware\ClientCredentialsMiddleware;
+use WyriMaps\BattleNet\Options;
 use function DI\factory;
 use function DI\get;
-use WyriMaps\BattleNet\AsyncClient;
 
 return [
-    AsyncClient::class => factory(function (string $key, LoopInterface $loop) {
-        return new AsyncClient($key, $loop);
+    AsyncClient::class => factory(function (string $authKey, string $authSecret, string $cacheKey, LoopInterface $loop) {
+        return AsyncClient::create(
+            $loop,
+            new ClientCredentials(
+                $authKey,
+                $authSecret
+            ),
+            [
+                FoundationOptions::TRANSPORT_OPTIONS => [
+                    TransportOptions::MIDDLEWARE => [
+                        ClientCredentialsMiddleware::class,
+                    ],
+                    TransportOptions::DEFAULT_REQUEST_OPTIONS => [
+                        ClientCredentialsMiddleware::class => [
+                            Options::API_TOKEN_CACHE_KEY => $cacheKey,
+                        ],
+                    ],
+                ],
+            ]
+        );
     })
-        ->parameter('key', get('config.wyrimaps.battlenet.key')),
+        ->parameter('authKey', get('config.wyrimaps.battlenet.auth.key'))
+        ->parameter('authSecret', get('config.wyrimaps.battlenet.auth.secret'))
+        ->parameter('cacheKey', get('config.wyrimaps.battlenet.cache.key')),
 ];
