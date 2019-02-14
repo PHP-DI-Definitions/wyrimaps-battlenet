3d3d4b000eae19aed2ccff22d9c637f9a6dbcb16
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-02-14 20:15:28 +0100

    Provide the interface not the implementation

--- etc/di/battlenet-client.php
+++ etc/di/battlenet-client.php
@@ -4,6 +4,7 @@ use ApiClients\Foundation\Options as FoundationOptions;
 use ApiClients\Foundation\Transport\Options as TransportOptions;
 use React\EventLoop\LoopInterface;
 use WyriMaps\BattleNet\AsyncClient;
+use WyriMaps\BattleNet\AsyncClientInterface;
 use WyriMaps\BattleNet\Authentication\ClientCredentials;
 use WyriMaps\BattleNet\Middleware\ClientCredentialsMiddleware;
 use WyriMaps\BattleNet\Options;
@@ -11,7 +12,7 @@ use function DI\factory;
 use function DI\get;
 
 return [
-    AsyncClient::class => factory(function (string $authKey, string $authSecret, string $cacheKey, LoopInterface $loop) {
+    AsyncClientInterface::class => factory(function (string $authKey, string $authSecret, string $cacheKey, LoopInterface $loop) {
         return AsyncClient::create(
             $loop,
             new ClientCredentials(
